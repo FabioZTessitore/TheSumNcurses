e2df58df498527303d433d06ea1f2e0169d5ddfe
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-01-04 14:34:21 +0100

    input quantity

--- uiadapter.c
+++ uiadapter.c
@@ -36,6 +36,7 @@ void UIAdapter_createWin(Quantity * const qty)
   getmaxyx(stdscr, ROWS, COLS);
 
   mvaddstr(ROWS-2, 5, "Press F2 to quit");
+  mvaddstr(ROWS-7, 5, "Add a quantity (int): ");
   refresh();
 
   quantityUI = quantityUI_createWin(qty);
@@ -43,9 +44,25 @@ void UIAdapter_createWin(Quantity * const qty)
 
 void UIAdapter_mainloop()
 {
-  int c = -1;
+  int ROWS, COLS;
+  getmaxyx(stdscr, ROWS, COLS);
+
+  int quantityToAdd = 0;
+  int c = '0';
 
   while (c != KEY_F(2)) {
-    c = getch();
+      c = '0';
+      while (c != '\n' && c != KEY_F(2)) {
+        if (c >= '0' && c <= '9') {
+          quantityToAdd = quantityToAdd*10 + (c-'0');
+          mvprintw(ROWS-7, 30, "%d", quantityToAdd);
+          refresh();
+        }
+        c = getch();
+      }
+    move(ROWS-7, 30);
+
+    quantityToAdd = 0;
+    mvaddstr(ROWS-7, 30, "\n");
   }
 }
